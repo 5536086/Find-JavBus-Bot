153a44afe8378dfed575f754b507a3fe19a4a3d9
5536086 <907328471@qq.com>
2023-05-12 20:41:00 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 export const ALLOWED_GROUPS = []
-export const BOT_TOKEN = '5045646424:AAGWgi85phnyyzsVZXChiuSxeV7X2Sjchn4'
+export const BOT_TOKEN = '6294973865:AAGjd8EQf-xsq0ZyxcMQqGv1h6BgdNsH2PA'
 export const ROBOT_NAME = '@寻龙'
